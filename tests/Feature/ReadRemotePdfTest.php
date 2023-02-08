ca03374bf35b343c0ec8eda20f0c3dbea05b9adc
Alpha Olomi <alphaolomi@gmail.com>
2023-02-08 19:09:37 +0300

    perf: minor chore update

--- tests/Feature/ReadRemotePdfTest.php
+++ tests/Feature/ReadRemotePdfTest.php
@@ -1,3 +1,8 @@
 <?php
 
-it('has readremotepdf page');
+it('can read remote pdf page from url');
+
+// it('can read remote pdf page from url', function () {
+// $pdf = new \Spatie\PdfToText\Pdf('https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf');
+// $this->assertEquals('Dummy PDF file', $pdf->getText());
+// });
